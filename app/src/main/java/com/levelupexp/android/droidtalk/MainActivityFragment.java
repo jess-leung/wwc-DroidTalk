ad6be3896bbb127d15e3318c37af6bc91fb8f33f
Jessie Leung <jleung@thoughtworks.com>
2016-07-21 18:42:01 +1000

    [Stephen/Jessie] events: on click add message

--- app/src/main/java/com/levelupexp/android/droidtalk/MainActivityFragment.java
+++ app/src/main/java/com/levelupexp/android/droidtalk/MainActivityFragment.java
@@ -6,7 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,6 +15,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 
@@ -22,7 +24,15 @@ public class MainActivityFragment extends Fragment {
     @BindView(R.id.message_list_view)
     ListView messageListView;
 
+    @BindView(R.id.message_button)
+    Button messageSendButton;
+
+    @BindView(R.id.message_input)
+    EditText messageInputBox;
+
     private Unbinder unbinder;
+    private List<String> list;
+    private ArrayAdapter arrayAdapter;
 
     public MainActivityFragment() {
     }
@@ -33,12 +43,21 @@ public class MainActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         unbinder = ButterKnife.bind(this, rootView);
 
-        List<String> list = new ArrayList<>();
+        list = new ArrayList<>();
         list.add("This is the first message");
         list.add("This is the second message");
-        ListAdapter listAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, list);
-        messageListView.setAdapter(listAdapter);
+        arrayAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, list);
+        messageListView.setAdapter(arrayAdapter);
         return rootView;
+    }
+
+    @OnClick(R.id.message_button)
+    public void submit() {
+        String inputText = messageInputBox.getText().toString();
+        list.add(inputText);
+        messageInputBox.setText("");
+        arrayAdapter.notifyDataSetChanged();
+
     }
 
     @Override public void onDestroyView() {
