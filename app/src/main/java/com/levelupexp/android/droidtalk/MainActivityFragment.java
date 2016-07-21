dc19be6f8a2ae6d9cb181bd190e379c023cac105
Jessie Leung <jleung@thoughtworks.com>
2016-07-21 18:25:09 +1000

    [Stephen/Jessie] fix butterknife import, added listview with messages

--- app/src/main/java/com/levelupexp/android/droidtalk/MainActivityFragment.java
+++ app/src/main/java/com/levelupexp/android/droidtalk/MainActivityFragment.java
@@ -5,11 +5,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+
 public class MainActivityFragment extends Fragment {
+
+    @BindView(R.id.message_list_view)
+    ListView messageListView;
+
+    private Unbinder unbinder;
 
     public MainActivityFragment() {
     }
@@ -17,6 +30,19 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
+
+        List<String> list = new ArrayList<>();
+        list.add("This is the first message");
+        list.add("This is the second message");
+        ListAdapter listAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, list);
+        messageListView.setAdapter(listAdapter);
+        return rootView;
+    }
+
+    @Override public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
